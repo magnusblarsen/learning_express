f3a75d4116836f59089851112059063a36ce5918
magnusblarsen <brml@itu.dk>
2022-01-25 11:13:46 +0100

    noget

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const http = require('http')
 
 const server = http.createServer((req,res)=>{
-    res.writeHead(200,{'content-type':'text/html'})
-    res.write('<h1>Home Page</h1>')
-    res.end()
+    //console.log(req.method);
+    //console.log(req.url);
+    const url = req.url;
+    if(url === '/'){ //home page
+        res.writeHead(200,{'content-type':'text/html'})
+        res.write('<h1>Home Page</h1>');
+        res.end();
+    } else if(url === '/about'){ //about page
+        res.writeHead(200,{'content-type':'text/html'})
+        res.write('<h1>About Page</h1>');
+        res.end();
+    } else{ //404
+        res.writeHead(404,{'content-type':'text/html'})
+        res.write('<h1>NOT FOUND!...</h1>');
+        res.end();
+    }
+    
+    
 })
 
 server.listen(5000)
