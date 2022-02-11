12d655f37dc53ebf2865494293e3a2c95965775d
magnusblarsen <magnusbrunlarsen00@gmail.com>
2022-02-11 08:49:01 +0100

    Tilføj ændring af app.js

--- app.js
+++ app.js
@@ -1,23 +1,45 @@
-const express = require('express');
+const expess = require('express');
 const res = require('express/lib/response');
-const path = require('path')
-
-const app = express(); 
+const {products} = require('./data')
 
 
-//setup static and middleware
-app.use(express.static('./public'))
+const app = expess();
 
-// app.get('/',(req,res)=>{
-//     res.sendFile(path.resolve(__dirname,'./navbar-app/index.html'))
-//     adding to static assets
-//     SSR
-// })
-
-app.all('*',(req,res)=>{
-    res.status(404).send('ressource not found')
+app.get('/', (req, res)=>{
+    res.send('<h1>Home Page</h1><a href="/api/products">Products</a>');
 })
 
-app.listen(5000,()=>{
-    console.log('server is listening on port 5000...');
+app.get('/api/products', (req,res)=>{
+    const productsMinusDescription = products.map((product)=>{
+        const {id, name, image} = product;
+        return {id,name,image};
+    })
+    
+    res.json(productsMinusDescription)
+})
+
+app.get('/api/products/:productID', (req,res)=>{
+    //console.log(req.params);
+
+    const {productID} = req.params;
+
+    const singleProduct = products.find(
+        (product)=> product.id === Number(productID));
+    
+    if(!singleProduct){
+        
+        return res.status(404).send('product does not exist')
+    }
+
+    return res.json(singleProduct)
+})
+
+app.get('/api/products/:productID/reviews/:reviewID', (req,res)=>{
+    console.log(req.params);
+    res.send('hello')
+
+})
+
+app.listen(5000, (req, res) => {
+    console.log('Server is listening on port 5000...');
 })
